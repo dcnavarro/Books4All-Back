89c07fae5347d8b6e5fb9eb38cc64ab1923f2da9
Roberto Ochoa <oc_005@hotmail.com>
2023-04-26 15:23:17 -0300

    borro imports inecesarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,17 @@
-const {Router} = require('express');
-const booksRouter = require ('./booksRouter')
-const reviewsRouter = require('./reviewsRouter');
-const usersRouter = require ('./usersRouter');
-const apiRouter = require('./apiRouter');
-const putRouter = require('./putRouter');
-const adminRouter = require('./adminRouter');
-const stripeResponseRouter = require('./stripeResponseRouter');
+const { Router } = require("express");
+const booksRouter = require("./booksRouter");
+const reviewsRouter = require("./reviewsRouter");
+const usersRouter = require("./usersRouter");
+const apiRouter = require("./apiRouter");
+const putRouter = require("./putRouter");
+const adminRouter = require("./adminRouter");
 const mainRouter = Router();
 
-mainRouter.use('/books', booksRouter)
-mainRouter.use('/reviews', reviewsRouter)
-mainRouter.use('/users', usersRouter)
-mainRouter.use('/api/stripe', apiRouter)
-mainRouter.use('/deleted', putRouter)
-mainRouter.use('/admin', adminRouter)
-mainRouter.use('/stripe', stripeResponseRouter)
+mainRouter.use("/books", booksRouter);
+mainRouter.use("/reviews", reviewsRouter);
+mainRouter.use("/users", usersRouter);
+mainRouter.use("/api/stripe", apiRouter);
+mainRouter.use("/deleted", putRouter);
+mainRouter.use("/admin", adminRouter);
 
 module.exports = mainRouter;
